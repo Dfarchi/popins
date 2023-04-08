f17e32c523de8d50e9ebe3201835bfa3ef35f455
Dfarchi <117561603+Dfarchi@users.noreply.github.com>
2023-04-08 18:34:50 +0300

    better in general

--- popins/popins_app/apps.py
+++ popins/popins_app/apps.py
@@ -4,3 +4,63 @@ from django.apps import AppConfig
 class PopinsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'popins_app'
+
+
+def sign_Up():
+    pass
+
+
+def create_nanny_profile():
+    pass
+
+
+def create_parent_profile():
+    pass
+
+
+def create_session():
+    pass
+
+
+def create_interest():
+    pass
+
+
+def create_review():
+    pass
+
+
+def update_profile():
+    pass
+
+
+def update_session():
+    pass
+
+
+def update_interest():
+    pass
+
+
+def update_reviwe():
+    pass
+
+
+def get_nanny():
+    pass
+
+
+def get_parent():
+    pass
+
+
+def get_session():
+    pass
+
+
+def get_all_interests():
+    pass
+
+
+def get_reviews():
+    pass
--- popins/popins_app/models.py
+++ popins/popins_app/models.py
@@ -1,75 +1,91 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Parent(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Profile(models.Model):
+    User_id = models.ForeignKey(User, db_column='user_id', on_delete=models.CASCADE)
+    is_parent = models.BooleanField(db_column='is_parent', default=False)
+    is_nanny = models.BooleanField(db_column='is_nanny', default=False)
     name = models.CharField(max_length=256, db_column='name', null=False, blank=False)
+    bio = models.TextField(max_length=256, db_column='bio', null=False, blank=False)
+    birth_year = models.IntegerField(db_column='birth_year', null=False)
     address = models.CharField(max_length=256, db_column='address', null=False, blank=False)
-    kids = models.CharField(max_length=256, db_column='kids', null=False, blank=False)
-    bio = models.CharField(max_length=256, db_column='bio', null=False, blank=False)
-    reviews = models.ForeignKey('Review', on_delete=models.RESTRICT, related_name='parent_reviews')
-    sessions = models.ForeignKey('Session', on_delete=models.RESTRICT, related_name='parent_sessions')
+    link = models.URLField(max_length=200, blank=True)
+    profile_pic = models.ImageField(upload_to='images/')
+
     # relations = models.ForeignKey('Relations', on_delete=models.RESTRICT, related_name='parent_relations')
     # phone_number = PhoneNumberField
-    birth_year = models.IntegerField(db_column='birth_year', null=False)
-
     def __str__(self):
         return self.name
 
     class Meta:
-        db_table = 'parents'
+        db_table = 'profiles'
 
 
-class Nanny(models.Model):
-    user = models.ForeignKey(User, on_delete=models.RESTRICT)
-    name = models.CharField(max_length=256, db_column='name', null=False, blank=False)
-    address = models.CharField(max_length=256, db_column='address', null=False, blank=False)
-    bio = models.CharField(max_length=256, db_column='bio', null=False, blank=False)
-    links = models.URLField(max_length=200, blank=True)
-    profile_pic = models.ImageField(upload_to='images/')
-    reviews = models.ForeignKey('Review', on_delete=models.RESTRICT, related_name='nanny_reviews')
-    sessions = models.ForeignKey('Session', on_delete=models.RESTRICT, related_name='nanny_sessions')
-    # relations = models.ForeignKey('Relations', on_delete=models.RESTRICT, related_name='nanny_relations')
-    # in_app_exp = ???
-    # phone_number = PhoneNumberField
-    birth_year = models.IntegerField(db_column='birth_year', null=False)
+class Session(models.Model):
+    user = models.OneToOneField(Profile, on_delete=models.CASCADE, default=None)
+    date = models.DateField(db_column='date', null=False, auto_now_add=True)
+    salary = models.IntegerField(db_column='salary', default=50)
+    note = models.TextField(max_length=256, db_column='note', null=False, blank=False)
+    has_happened = models.BooleanField(null=False, default=False)
 
     def __str__(self):
-        return self.name
+        return f"{self.user.name} on {self.date}"
 
     class Meta:
-        db_table = "nanny's"
+        db_table = 'sessions'
 
 
-class Interests(models.Model):
-    nanny = models.ForeignKey('Nanny', on_delete=models.RESTRICT)
-    session = models.ForeignKey('Session', on_delete=models.RESTRICT)
-    note = models.CharField(max_length=256, db_column='note', null=False, blank=False)
+class Interest(models.Model):
+    user = models.OneToOneField(Profile, on_delete=models.RESTRICT)
+    session = models.ManyToManyField(Session,db_table='Session_Interest')
+    note = models.TextField(max_length=256, db_column='note', null=False, blank=False)
+    talked = models.BooleanField(null=False, blank=False)
+    status = models.BooleanField(null=False, blank=False)
 
     class Meta:
-        db_table = "interests"
+        db_table = "interest"
 
 
 class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    user = models.OneToOneField(Profile, on_delete=models.RESTRICT)
     review = models.TextField(db_column='review', null=False)
-    review_date = models.DateField(db_column='date', null=False, default='1900-01-01')
+    review_date = models.DateField(db_column='date', null=False, auto_now_add=True)
 
     class Meta:
         db_table = "reviews"
 
 
-class Session(models.Model):
-    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
-    date = models.DateField(db_column='date', null=False, default='1900-01-01')
-    salary = models.IntegerField(null=True, blank=0)
-    note = models.CharField(max_length=256, db_column='note', null=False, blank=False)
-    has_happened = models.BooleanField(null=True, blank=False)
-
-    # roll = models.TextField(db_column='roll')
-
-    def __str__(self):
-        return f"{self.parent.name} on {self.date}"
-
-    class Meta:
-        db_table = 'sessions'
+"MERGEd TABLES"
+# class Parent(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=256, db_column='name', null=False, blank=False)
+#     kids = models.CharField(max_length=256, db_column='kids', null=False, blank=False)
+#
+#
+#     def __str__(self):
+#         return self.name
+#
+#     class Meta:
+#         db_table = 'users'
+#
+#
+# class Nanny(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.RESTRICT)
+#     name = models.CharField(max_length=256, db_column='name', null=False, blank=False)
+#     address = models.CharField(max_length=256, db_column='address', null=False, blank=False)
+#     bio = models.CharField(max_length=256, db_column='bio', null=False, blank=False)
+#
+#     """create links table"""
+#     sessions = models.ForeignKey('Session', on_delete=models.RESTRICT, related_name='nanny_sessions')
+#     # relations = models.ForeignKey('Relations', on_delete=models.RESTRICT, related_name='nanny_relations')
+#     # in_app_exp = ???
+#     # phone_number = PhoneNumberField
+#     birth_year = models.IntegerField(db_column='birth_year', null=False)
+#
+#     def __str__(self):
+#         return self.name
+#
+#     class Meta:
+#         db_table = "nannys"
+#
